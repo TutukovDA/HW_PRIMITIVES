bf6829f07bb1add7b6c4ee1ded087b32e738f895
Dmitriy Tyutyukov <dmitriy.tyutyukov@ifellow.ru>
2023-12-03 08:33:25 +0300

    add calculation

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int price = 13676;// Стоимость билета
+        int bonus = price / 20;// Механизм расчета бонусных миль
+        System.out.println("С покупки билета Вам полагается " + bonus + " бонусных миль");
+    }
 }
